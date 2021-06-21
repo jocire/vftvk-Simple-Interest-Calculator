8298ee8aef02e3b819c4c1b0f67227a2cc213b6c
Andreas Walter <39363506+jocire@users.noreply.github.com>
2021-06-21 17:35:57 -0500

    Update script.js
    Added form JS

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
     
+    var principal = document.getElementById("principal").value;
+    /* Validate that a positive number is given by user */
+        if (principal <= 0) {
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();   
+        } else {
+            var rate = document.getElementById("rate").value;
+            var years = document.getElementById("years").value;
+            /* Calculate Interest */
+            var interest = principal * years * rate /100;
+            /* Convert Num of Years into actual Year */
+            var year = new Date().getFullYear()+parseInt(years); 
+            /* Display Results */
+            document.getElementById("result").innerHTML=
+            "<p>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\></p>";       
+        }
 }
-        
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    /* Change Rate Value when moving Slider */
+    document.getElementById("rate_val").innerText=rateval;
+}      
